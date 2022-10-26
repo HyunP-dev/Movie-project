d9bb22d2c97df20a4b7a2219d9fd6dcf3264dc00
kimwanyoung <dhks2869@gmail.com>
2022-10-26 22:50:37 +0900

    setting

--- back/src/main/movie-project/src/App.tsx
+++ back/src/main/movie-project/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [test, setTest] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('/api/test')
+      .then(response => setTest(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return <div>백엔드에서 가져온 데이터입니다 : {test}</div>;
 }
 
 export default App;
