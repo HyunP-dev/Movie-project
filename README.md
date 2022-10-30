6276b840bcebc12de21d6710ce94be5e88d188fe
Jae woo kim <ta3844@naver.com>
2022-10-30 12:24:02 +0900

    영화글 컨트롤러 세팅

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 test
 
+<<<<<<< HEAD
+=======
+kim test
+
+pull test
+>>>>>>> 0b9cd85d6e11c55fb85e310a8786a1465b350bb1
--- back/src/main/java/com/movie/back/controller/MainController.java
+++ back/src/main/java/com/movie/back/controller/MainController.java
@@ -1,9 +1,17 @@
 package com.movie.back.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.movie.back.dto.MovieDTO;
+import com.movie.back.entity.Movie;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/mvi")
 public class MainController {
 
 
@@ -11,5 +19,36 @@ public class MainController {
     public String get(){
         return "요청성공!??!?!?!";
     }
+
+    @ApiOperation(value = "MOVIE All GET",notes = "GET 방식으로 전체조회")
+    @GetMapping(value = "/",consumes = MediaType.APPLICATION_JSON_VALUE)    //전체 데이터 조회
+    public ResponseEntity<List<MovieDTO>> readAll(){    //todo: PageResponse를 만들어서 page 개체를 던질예정
+            return ResponseEntity.ok(new ArrayList<MovieDTO>());                            //PageResponse  안에는 페이지가 있음
+    }                                                                                       //TODO: PageRequest 객체를 통해 받을 예정
+    @ApiOperation(value = "MOVIE ONE GET",notes = "GET 방식으로 하나조회")
+    @GetMapping(value="/{movieId}")
+    public ResponseEntity<MovieDTO> read(@PathVariable("movieId") Long movieId){
+            return ResponseEntity.ok(new MovieDTO());
+    }
+    @ApiOperation(value = "MOVIE POST",notes = "POST 요청")
+    @PostMapping(value = "",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> register(@RequestBody MovieDTO movieDTO){
+            return ResponseEntity.ok("post");
+    }
+    @ApiOperation(value = "MOVIE PUT",notes = "PUT 요청")
+    @PutMapping(value = "/{movieId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MovieDTO> modify(
+            @PathVariable("movieId") Long movieId,
+            @RequestBody MovieDTO movieDTO){
+        return ResponseEntity.ok(new MovieDTO());
+    }
+
+    @ApiOperation(value = "MOVIE DELETE",notes = "DELETE 요청")
+    @DeleteMapping(value = "/{movieId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> remove(@PathVariable("movieId") Long movieId){
+            return ResponseEntity.ok(1L);   //todo: 삭제한 PK가 날아가게
+    }
+
+
 
 }
