4a0c831d079bb497380bf10a86afc095140fbaf7
kimwanyoung <dhks2869@gmail.com>
2022-11-07 18:46:56 +0900

    ADD:Carousel

--- back/src/main/movie-project/src/pages/Main/Main.tsx
+++ back/src/main/movie-project/src/pages/Main/Main.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+import { Carousel, CarouselItem } from 'react-round-carousel';
+import 'react-round-carousel/src/index.css';
 
 const Main = () => {
   const [movie, setMovie] = useState([]);
@@ -8,23 +10,58 @@ const Main = () => {
     fetch('/mvi/box')
       .then(res => res.json())
       .then(data => setMovie(data));
-  }, []);
 
-  return (
-    <MainWrapper>
-      <MainTopText>Lorem ipsum</MainTopText>
-      <MovieListWrapper>
-        {movie?.map((props, idx) => (
+    console.log(movie);
+  }, []);
+  const movieTest: CarouselItem[] = movie?.map(
+    (props: string, index: number) => ({
+      alt: 'movie poster',
+      image: props.image,
+      content: (
+        <div>
+          <strong>{props.title}</strong>
           <Link
-            key={idx}
+            key={index}
             to={'/detail'}
             state={{ image: props?.image, title: props?.title }}
           >
             <MovieImg src={props.image} alt="1" />
           </Link>
-        ))}
-      </MovieListWrapper>
-    </MainWrapper>
+        </div>
+      ),
+    })
+  );
+  // const MovieCarousel: CarouselItem[] = Array(8)
+  //   .fill('')
+  //   .map((props: string, index: number) => ({
+  //     alt: 'A random photo',
+  //     image: `https://picsum.photos/${210 + index}`,
+  //     content: (
+  //       <div>
+  //         <strong>Round Carousel</strong>
+  //         <span>Slide number {index + 1}</span>
+  //       </div>
+  //     ),
+  //   }));
+
+  return (
+    <>
+      <MainWrapper>
+        <MainTopText>Lorem ipsum</MainTopText>
+        <MovieListWrapper>
+          {/* {movie?.map((props, idx) => (
+            <Link
+              key={idx}
+              to={'/detail'}
+              state={{ image: props?.image, title: props?.title }}
+            >
+              <MovieImg src={props.image} alt="1" />
+            </Link>
+          ))} */}
+        </MovieListWrapper>
+      </MainWrapper>
+      <Carousel items={movieTest} itemWidth={500} />
+    </>
   );
 };
 
@@ -46,8 +83,7 @@ const MainTopText = styled.div`
 
 const MovieListWrapper = styled.div`
   width: 100%;
-  height: 80vh;
-  border: 1px solid black;
+  height: 30vh;
 `;
 
 const MovieImg = styled.img`
