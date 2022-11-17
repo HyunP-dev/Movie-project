6c1151a56bc901a85defc45c39dd5c0cd873a30d
Jae woo kim <ta3844@naver.com>
2022-11-17 21:22:49 +0900

    pull 받을준비

--- back/src/test/java/com/movie/back/repository/MemberRepositoryTest.java
+++ back/src/test/java/com/movie/back/repository/MemberRepositoryTest.java
@@ -26,4 +26,11 @@ class MemberRepositoryTest {
                         .nickName("닉네읨")
                 .build());
     }
+
+    @Test
+    void test2(){
+        String str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2ODcwNTgsImlhdCI6MTY2ODY4Njk5OCwiZW1haWwiOiJ1c2VyIn0.-KyGdJXOt2jhBW5oOx0JzNauzSSr7jQBKLx1Ag19QI8";
+        String str2 ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2ODcxNTYsImlhdCI6MTY2ODY4NzA5NiwiZW1haWwiOiJ1c2VyIn0.tF0w5WKjukjnk3lujfeov2jjXB-KhsA-Ip8yOdO03q0"
+
+    }
 }
